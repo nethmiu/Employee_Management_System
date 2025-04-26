d2108e3557f5d49f476106e4aa87c57812a92545
Nethmi Umaya Silva <nethmiumayapc@gmail.com>
2025-04-26 13:29:04 +0530

    Update leaves.js

--- Backend/employee/routes/leaves.js
+++ Backend/employee/routes/leaves.js
@@ -12,8 +12,8 @@ const transporter = nodemailer.createTransport({
     port: 587,
     secure: false, 
     auth: {
-        user: "assigmentgroupy@gmail.com",
-        pass: "iehl zcwp pdmy anld",
+        user: "<your@gmail.com>",
+        pass: "###",
     },
 });
 
